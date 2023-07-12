b0ddc4c9c885b13adfb2aa52c6f611ded6a19cd2
KKDinev20 <KKDinev20@codingburgas.bg>
2023-07-13 00:43:30 +0300

    Add Alternative Website

--- cv-builder/src/App.js
+++ cv-builder/src/App.js
@@ -1,13 +1,15 @@
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Banner } from "./components/Banner";
-import { NavBar } from "./components/NavBar";
+import NavBar from './components/NavBar';
+
+
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-       <NavBar />
-       <Banner />
+      <NavBar />
+      <Banner />
     </div>
   );
 }
